ebbf0ea6987741531b6368e68bd97a848b5632da
JW Moon <moon910904@gmail.com>
2022-01-25 16:22:18 +0900

    Initial Commit

---
+++ schedule_words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  schedule_words
+//
+//  Created by JW Moon on 2022/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
